d4241d5dbc2d697795e65361366b344fe2405a98
Mykhailo Humeniuk <mykhailohumeniuk@miha-air-2.myfiosgateway.com>
2021-10-26 17:25:16 -0700

    watch for changes

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ var path = require('path');
 var reload = require('reload');
 var logger = require('morgan');
 var watch = require('node-watch');
+var ncp = require('ncp').ncp;
 
 const app = express();
 
-const localDirname = './'; //'/public';
+const localDirname = '/public';
+const staticDirname = '/static';
+const sourceDirname = '/src';
 
-//move to config
+//TODO: move to config, use nodemon
 const publicDir = path.join(__dirname, localDirname);
+const srcDir = path.join(__dirname, sourceDirname);
+const staticDir = path.join(publicDir, staticDirname);
+console.log(staticDir);
+
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,15 +26,33 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(publicDir, 'index.html'))
 });
 
-app.use(express.static(path.resolve(publicDir, './src')));
+app.use(express.static(path.resolve(publicDir, './static')));
 
 reload(app).then(function (reloadReturned) {
-  // Reload started, start web server
-  server.listen(app.get('port'), function () {
-    console.log('Web server listening on port ' + app.get('port'))
-  })
+  startServer();
 }).catch(function (err) {
   console.error('Reload could not start, could not start the app', err)
-})
+});
+
+//watch for changes copy code, restart server
+watch(srcDir, { recursive: true }, function (f, curr, prev) {
+  console.log('source code, restarting server...');
+  ncp(srcDir, staticDir, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    server.close(function() {
+      console.log('starting') 
+      startServer();
+    });
+  });
+});
+
+function startServer() {
+  server.listen(app.get('port'), function () {
+    console.log('Web server listening on port ' + app.get('port'))
+  });
+}
+
 
 var server = http.createServer(app);
