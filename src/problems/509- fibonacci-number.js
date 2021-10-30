a49b5029ef5a2dce59b96b533d248150093b8730
mhumeniuk <miha.000@gmail.com>
2021-10-30 09:45:23 -0700

    509: Fix fibonacci

--- src/problems/509- fibonacci-number.js
+++ src/problems/509- fibonacci-number.js
@@ -3,13 +3,18 @@
  * @return {number}
  */
 
+//0 <= n <= 30
+
 var fib = function(n) {
   let memo = {};
   function fibNum(n, memo) { 
+    if (n === 0) {
+      return 0;
+    }
     if (memo[n]) { 
       return memo[n];
     }
-    if (n <= 1 || n === 2) { 
+    if (n === 1 || n === 2) { 
       return 1; 
     } 
     memo[n] = fibNum(n - 1, memo) + fibNum(n - 2, memo);
