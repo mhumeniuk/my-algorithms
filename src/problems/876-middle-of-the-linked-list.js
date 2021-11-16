a745f76f56bca8384ddc9358b24968998367bdc7
mhumeniuk <miha.000@gmail.com>
2021-11-15 19:05:17 -0800

    897: middle of the linked list

---
+++ src/problems/876-middle-of-the-linked-list.js
@@ -0,0 +1,84 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+//TODO: solve as two pointer;
+ var middleNode = function(head) {
+  let counter = 1;
+  let currentNode = head;
+  let prevNode;
+  
+  while (currentNode.next) {
+    counter++;
+    currentNode = currentNode.next;
+  }
+  
+  currentNode = head;
+  let middleCount = Math.ceil(counter/2);
+  let isTwoMiddle = counter % 2 == 0;
+  
+  counter = 1;
+  while (currentNode.next) {
+    if (counter == middleCount) {
+      if (isTwoMiddle) {
+        return currentNode.next;
+      }
+      else {
+        return currentNode; 
+      }
+    }
+    else {
+      counter++;
+      currentNode = currentNode.next;
+    }
+  }
+  return currentNode;
+};
+
+var middleNodeAlt = function(head) {
+  let counter = 1;
+  let currentNode = head;
+  let prevNode;
+
+  while (currentNode.next) {
+    counter++;
+    currentNode = currentNode.next;
+  }
+  
+  currentNode = head;
+  let middleCount = Math.floor(counter/2);
+
+  while (middleCount !== 0) {
+    currentNode = currentNode.next;
+    middleCount--;
+  }
+  return currentNode;
+};
+
+//Initial bad idea reversing linked list. Since I though it i need two lists for "Two Pointer" problem
+/*while (currentNode !== null) {
+  let nextNode;
+  if (typeof prevNode !== "undefined") {
+      nextNode = currentNode.next;
+      currentNode.next = prevNode;
+  }
+  else if (currentNode.next === null) {
+      nextNode = currentNode.next;
+      currentNode.next = head;
+  }
+  else {
+      nextNode = currentNode.next;
+      currentNode.next = null;
+  }
+
+  prevNode = currentNode;
+  currentNode = nextNode    
+}*/  
