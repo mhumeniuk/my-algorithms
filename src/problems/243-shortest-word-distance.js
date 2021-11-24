980b7e13c6615bd8442f75b71fe7ec36bbf1bd95
mhumeniuk <miha.000@gmail.com>
2021-11-23 20:52:31 -0800

    243: Shortest Word Distance

---
+++ src/problems/243-shortest-word-distance.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} wordsDict
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+//TODO: literaly write code on paper first!!
+var shortestDistance = function(wordsDict, word1, word2) {
+    let steps = 0;
+    let distances = [];
+    let hasStarted = false;
+    let startingWord = null;
+    for (let i = 0; i < wordsDict.length; i++) {
+        if(wordsDict[i] == word1 || wordsDict[i] == word2) {
+           if (startingWord !== null) { //we went to the end
+               if (startingWord !== wordsDict[i]) {
+                   distances.push(steps);
+               }
+               steps = 1;
+               startingWord = wordsDict[i];
+           }
+           else {
+               steps = 1;
+               startingWord = wordsDict[i];
+           }
+        }
+        else if (startingWord !== null) {
+            steps ++;
+        }
+    }
+    return Math.min(...distances);
+};
